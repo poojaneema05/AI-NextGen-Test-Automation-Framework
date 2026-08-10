e59001ad60cf79b21014ef3b91d7ed91fb779948
Pooja N <pooja9neema@gmail.com>
2026-08-09 22:25:32 -0700

    Add login page object and test

---
+++ tests/web/login.spec.ts
@@ -0,0 +1,14 @@
+import { test } from "@fixtures/framework.fixture";
+
+test("Verify successful login", async ({ loginPage }) => {
+
+    await loginPage.open();
+
+    await loginPage.login(
+        "student",
+        "Password123"
+    );
+
+    await loginPage.verifySuccessfulLogin();
+
+});
